ea55308f9c7423b125817faf58fea15ea291496d
alirazamakal <121793094+alirazamakal@users.noreply.github.com>
2023-04-05 15:22:42 +0500

    some section is done

--- main.js
+++ main.js
@@ -17,3 +17,28 @@ const closeNav = () => {
 
 
 closeNavBtn.addEventListener('click', closeNav);
+
+
+//  TESTIMONIALS SECTION (Swiper js)
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    
+    //  Responsive Breakpoints
+    breakpoints: {
+        // when window width is >=600px
+        600: {
+            slidesPerView: 2
+        },
+
+    // when window width is >=1024px
+    1024: {
+        slidesPerView: 3
+    }
+  }
+    
+  });
